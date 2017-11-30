c25c8914fe123c1990537aeea8e097ef2512bb9c
SimpleTrousers <simpletrousers2@gmail.com>
2017-11-29 22:28:35 -0700

    Added some mysql connection code.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var mysql = require('mysql');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +9,15 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/stumper'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// Configure server, try to connect to database on database port, not server
+var con = mysql.createConnection({
+  host: "157.201.228.126",
+  user: "stumperadmin",
+  password: "cit380game"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
